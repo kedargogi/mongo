ab801861f90f9c3d652e6adeb0a8a4c9c93ad993
kedargogi <kedargogi420@gmail.com>
2022-09-29 18:19:58 +0530

    db commit update

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://kedar91:password@1991@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
   )
     .then(client => {
       console.log('Connected!');
